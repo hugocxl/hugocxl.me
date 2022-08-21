0b89d21c5e9af72ebd01cea8f2537e2f3798bc0b
hcorta <corta.hugo@gmail.com>
2022-08-21 19:55:36 +0200

    Refined home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,39 @@
 // Dependencies
+import { Link, Stack, Typography } from '@mui/material'
 import * as React from 'react'
 
-const Home = ({}) => {
-  return <span>{'home'}</span>
+const Home = () => {
+  return (
+    <Stack direction={'column'} height={'100%'} justifyContent={'center'}>
+      <Typography variant={'h3'}>{`Hi 👋 – I’m Hugo`}</Typography>
+      <Typography color={'textSecondary'}>
+        <strong>Software developer</strong> at <strong>@Metasoccer</strong>
+      </Typography>
+      <Typography marginTop={4}>
+        {'I love creating useful products.'}
+      </Typography>
+      <Typography>
+        Wanna reach out? You can find me on{' '}
+        <Link
+          href={`https://github.com/hcorta`}
+          title={`GitHub @hcorta`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {'GitHub'}
+        </Link>{' '}
+        or{' '}
+        <Link
+          href={`https://twitter.com/hcorta`}
+          title={`Twitter @hcorta`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {'Twitter'}
+        </Link>
+      </Typography>
+    </Stack>
+  )
 }
 
 export default Home
