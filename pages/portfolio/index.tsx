17b6bf87cdf89451cf07d26972599dbbd1fe4ca9
hcorta <corta.hugo@gmail.com>
2022-08-22 12:57:26 +0200

    Masonry layout in portfolio

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -5,11 +5,15 @@ import * as path from 'path'
 import { getMetaFromDocsDir } from '../../utils'
 
 // Components
+import NextImage from 'next/image'
+import NextLink from 'next/link'
 import { Page, ArticlesList } from '../../components'
-import Masonry from '@mui/lab/Masonry'
+import { Masonry } from '@mui/lab'
+import { Paper, Typography, Chip, Stack } from '@mui/material'
 
 // Constants
-const PORTFOLIO_DIR = path.join(process.cwd(), 'docs', 'portfolio')
+const BASE_PORTFOLIO_PATH = 'portfolio'
+const PORTFOLIO_DIR = path.join(process.cwd(), 'docs', BASE_PORTFOLIO_PATH)
 
 export async function getStaticProps() {
   const projects = getMetaFromDocsDir(PORTFOLIO_DIR)
@@ -28,7 +32,35 @@ export default function Portfolio({ projects }) {
       title={'Portfolio'}
       description={`Software developer and open source author, I believe in proof of work. These are my popular works.`}
     >
-      <ArticlesList articles={projects} baseUrl={'portfolio'} />
+      <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 3, xl: 3 }} spacing={2}>
+        {projects.map((project) => (
+          <NextLink href={`/${BASE_PORTFOLIO_PATH}/${project.slug}`}>
+            <Paper
+              key={project.slug}
+              elevation={2}
+              sx={{
+                cursor: 'pointer',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: 2
+              }}
+            >
+              <img src={project.meta.bannerImage} alt={project.meta.title} />
+              <Typography variant={'h6'} mt={2}>
+                {project.meta.title}
+              </Typography>
+              <Typography variant={'body2'}>
+                {project.meta.description}
+              </Typography>
+              <Stack spacing={1} mt={1}>
+                {project.meta.tags.map((tag) => (
+                  <Chip label={tag} />
+                ))}
+              </Stack>
+            </Paper>
+          </NextLink>
+        ))}
+      </Masonry>
     </Page>
   )
 }
