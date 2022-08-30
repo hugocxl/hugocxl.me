33ac59c32de1ccf721cb7ae8977272831bcd3ee9
hcorta <corta.hugo@gmail.com>
2022-08-30 19:07:36 +0200

    Home screen remove animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 // Components
-import { Button, Fade, Link, Stack, Typography } from '@mui/material'
+import { Button, Link, Stack, Typography } from '@mui/material'
 import { Page } from 'src/components'
 import NextLink from 'next/link'
 
@@ -18,59 +18,57 @@ const HomePage: NextPage = () => {
       description={HOME_PAGE_DESCRIPTION}
       showHeader={false}
     >
-      <Fade in={true} timeout={1000}>
-        <Stack
-          spacing={0}
-          alignItems={'flex-end'}
-          justifyContent={'center'}
-          height={'100%'}
-          direction={'column'}
-        >
-          <Typography
-            gutterBottom={false}
-            variant={'h4'}
-            component={'h1'}
-          >{`Hi 👋 – I’m Hugo`}</Typography>
-          <Typography variant={'subtitle1'} component={'h2'}>
-            {HOME_PAGE_DESCRIPTION}
-          </Typography>
+      <Stack
+        spacing={0}
+        alignItems={'flex-end'}
+        justifyContent={'center'}
+        height={'100%'}
+        direction={'column'}
+      >
+        <Typography
+          gutterBottom={false}
+          variant={'h4'}
+          component={'h1'}
+        >{`Hi 👋 – I’m Hugo`}</Typography>
+        <Typography variant={'subtitle1'} component={'h2'}>
+          {HOME_PAGE_DESCRIPTION}
+        </Typography>
 
-          <Typography marginTop={4}>
-            {'I love creating useful products.'}
-          </Typography>
-          <Typography>
-            Wanna reach out? You can find me on{' '}
-            <Link
-              href={`https://github.com/hcorta`}
-              title={`GitHub @hcorta`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {'GitHub'}
-            </Link>{' '}
-            or{' '}
-            <Link
-              href={`https://twitter.com/hcorta`}
-              title={`Twitter @hcorta`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {'Twitter'}
-            </Link>
-            {'.'}
-          </Typography>
+        <Typography marginTop={4}>
+          {'I love creating useful products.'}
+        </Typography>
+        <Typography>
+          Wanna reach out? You can find me on{' '}
+          <Link
+            href={`https://github.com/hcorta`}
+            title={`GitHub @hcorta`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {'GitHub'}
+          </Link>{' '}
+          or{' '}
+          <Link
+            href={`https://twitter.com/hcorta`}
+            title={`Twitter @hcorta`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {'Twitter'}
+          </Link>
+          {'.'}
+        </Typography>
 
-          <Stack spacing={1} mt={1}>
-            {PAGES.map(({ path, label }) => (
-              <NextLink href={path}>
-                <Button size={'small'} variant={'outlined'}>
-                  {label}
-                </Button>
-              </NextLink>
-            ))}
-          </Stack>
+        <Stack spacing={1} mt={1}>
+          {PAGES.map(({ path, label }) => (
+            <NextLink href={path}>
+              <Button size={'small'} variant={'outlined'}>
+                {label}
+              </Button>
+            </NextLink>
+          ))}
         </Stack>
-      </Fade>
+      </Stack>
     </Page>
   )
 }
