d8fd4cd594a3279f2dc484aea5ecd9c53e76cc6e
hcorta <corta.hugo@gmail.com>
2022-08-30 18:54:57 +0200

    Main color for light theme

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -15,7 +15,7 @@ export const lightTheme: ThemeOptions = deepmerge(
         main: '#000000'
       },
       secondary: {
-        main: '#2564eb'
+        main: '#3292ff'
       },
       text: {
         primary: '#4d4d4d',
