569af3feaca864ba4255bbcc22fceed33dfdec2e
hcorta <corta.hugo@gmail.com>
2022-09-22 20:08:16 +0200

    Responsive navbar

--- src/components/Layout/components/NavBar/NavBar.tsx
+++ src/components/Layout/components/NavBar/NavBar.tsx
@@ -2,19 +2,23 @@
 import {
   IconButton,
   Link,
+  Dialog,
   Stack,
   Tooltip,
   useMediaQuery,
-  useTheme
+  useTheme,
+  Typography,
+  Divider
 } from '@mui/material'
 import { IoSunnyOutline, IoMoonOutline } from 'react-icons/io5'
 import NextLink from 'next/link'
+import { VscMenu } from 'react-icons/vsc'
 
 // Constants
 import { PAGES } from 'src/constants'
 
 // Types
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { NavBarProps } from './NavBar.types'
 
 // Hooks
@@ -24,9 +28,14 @@ import { useRouter } from 'next/router'
 export const NavBar: FC<NavBarProps> = () => {
   const { pathname } = useRouter()
   const [mode, toggleMode] = useThemeMode()
+  const [openMenu, setOpenMenu] = useState<boolean>(false)
   const theme = useTheme()
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'))
   const isDarkMode = mode === 'dark'
+
+  function onClickMenu() {
+    setOpenMenu((prev) => !prev)
+  }
 
   function PagesLinks() {
     return (
@@ -36,21 +45,13 @@ export const NavBar: FC<NavBarProps> = () => {
 
           return (
             <NextLink href={path} key={path}>
-              <Tooltip title={label} placement={'bottom'}>
-                {isSmallScreen ? (
-                  <IconButton {...(isActive && { color: 'secondary' })}>
-                    <Icon />
-                  </IconButton>
-                ) : (
-                  <Link
-                    variant={'subtitle1'}
-                    color={'inherit'}
-                    gutterBottom={false}
-                  >
-                    {label}
-                  </Link>
-                )}
-              </Tooltip>
+              <Link
+                variant={'subtitle1'}
+                color={!isActive ? 'inherit' : 'secondary'}
+                gutterBottom={false}
+              >
+                {label}
+              </Link>
             </NextLink>
           )
         })}
@@ -68,22 +69,66 @@ export const NavBar: FC<NavBarProps> = () => {
     )
   }
 
+  function Menu() {
+    return (
+      <>
+        <IconButton onClick={onClickMenu} sx={{ bgcolor: 'action.hover' }}>
+          <VscMenu />
+        </IconButton>
+
+        <Dialog onClose={onClickMenu} open={openMenu} fullScreen>
+          <Stack
+            height={'100%'}
+            spacing={3}
+            alignItems={'center'}
+            justifyContent={'center'}
+            direction={'column'}
+            onClick={onClickMenu}
+          >
+            <Divider sx={{ width: '100%' }} />
+            {PAGES.map(({ label, path, icon: Icon }) => {
+              const isActive = pathname.startsWith(path)
+
+              return (
+                <>
+                  <NextLink href={path} key={path}>
+                    <Link
+                      sx={{ display: 'flex', alignItems: 'center' }}
+                      color={!isActive ? 'primary' : 'secondary'}
+                    >
+                      <Icon />
+                      <Typography ml={2} gutterBottom={false}>
+                        {label}
+                      </Typography>
+                    </Link>
+                  </NextLink>
+                  <Divider sx={{ width: '100%' }} />
+                </>
+              )
+            })}
+          </Stack>
+        </Dialog>
+      </>
+    )
+  }
+
   return (
     <Stack
       px={4}
       py={6}
+      {...(isSmallScreen && { px: 1, py: 3 })}
       component={'header'}
       alignItems={'center'}
       display={'flex'}
       justifyContent={'space-between'}
     >
       <NextLink href='/'>
-        <Link variant={'h6'} color={'primary'} gutterBottom={false}>
+        <Link variant={'h6'} color={'primary'} mb={0}>
           Hugo Corta
         </Link>
       </NextLink>
       <Stack spacing={2}>
-        <PagesLinks />
+        {isSmallScreen ? <Menu /> : <PagesLinks />}
         <ThemeButton />
       </Stack>
     </Stack>
