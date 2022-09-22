1c5da836c14b38ff56741ccaeda311386120e189
hcorta <corta.hugo@gmail.com>
2022-09-22 18:52:17 +0200

    Main layout changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,11 +3,14 @@ import { NavBar } from './components'
 import { Container } from '@mui/material'
 
 // Types
-import { LayoutProps } from './Layout.types'
 import { FC } from 'react'
 
 // Styles
 import styles from './Layout.module.css'
+
+export interface LayoutProps {
+  children: React.ReactNode
+}
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,44 +21,45 @@ const HomePage: NextPage = () => {
       title={HOME_PAGE_TITLE}
       description={HOME_PAGE_DESCRIPTION}
       showHeader={false}
+      display={'flex'}
+      justifyContent={'center'}
+      flexDirection={'column'}
     >
-      <Stack justifyContent={'center'} direction={'column'} pt={12}>
-        <Typography
-          gutterBottom={false}
-          variant={'h2'}
-          component={'span'}
-          className={'gradient-text'}
-        >{`Hi, I’m Hugo`}</Typography>
-        <Typography variant={'h4'} component={'span'}>
-          {HOME_PAGE_DESCRIPTION}
-        </Typography>
-        <Typography fontSize={'20px'} lineHeight={'32px'} mb={4}>
-          This website is supposed to be part portfolio, part rambling space. A
-          place where I’ll try to show my skills and projects, check it out!
-        </Typography>
+      <Typography
+        gutterBottom={false}
+        variant={'h2'}
+        component={'span'}
+        className={'gradient-text'}
+      >{`Hi, I’m Hugo`}</Typography>
+      <Typography variant={'h4'} component={'span'}>
+        {HOME_PAGE_DESCRIPTION}
+      </Typography>
+      <Typography fontSize={'20px'} lineHeight={'32px'} mb={4}>
+        This website is supposed to be part portfolio, part rambling space. A
+        place where I’ll try to show my skills and projects, check it out!
+      </Typography>
 
-        <Stack spacing={1}>
-          <LinkIconButton
-            icon={BsFillEnvelopeFill}
-            href={`mailto: corta.hugo@gmail.com`}
-            title={`Mail corta.hugo@gmail.com`}
-          />
-          <LinkIconButton
-            icon={BsGithub}
-            href={`https://github.com/hcorta`}
-            title={`GitHub @hcorta`}
-          />
-          <LinkIconButton
-            icon={BsTwitter}
-            href={`https://twitter.com/`}
-            title={`Twitter @hcorta`}
-          />
-          <LinkIconButton
-            icon={BsLinkedin}
-            href={`https://www.linkedin.com/in/hugocorta`}
-            title={`LinkedIn @hugocorta`}
-          />
-        </Stack>
+      <Stack spacing={1}>
+        <LinkIconButton
+          icon={BsFillEnvelopeFill}
+          href={`mailto: corta.hugo@gmail.com`}
+          title={`Mail corta.hugo@gmail.com`}
+        />
+        <LinkIconButton
+          icon={BsGithub}
+          href={`https://github.com/hcorta`}
+          title={`GitHub @hcorta`}
+        />
+        <LinkIconButton
+          icon={BsTwitter}
+          href={`https://twitter.com/`}
+          title={`Twitter @hcorta`}
+        />
+        <LinkIconButton
+          icon={BsLinkedin}
+          href={`https://www.linkedin.com/in/hugocorta`}
+          title={`LinkedIn @hugocorta`}
+        />
       </Stack>
     </Page>
   )
