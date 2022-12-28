eea72dc024c13083623d88fe2516c57b09c6580b
hcorta <corta.hugo@gmail.com>
2022-12-28 17:30:49 +0100

    Fixes

--- src/pages/books.tsx
+++
@@ -1,5 +0,0 @@
-import { BooksServer } from '@/backend/modules/books'
-import { Books } from '@/frontend/modules/books'
-
-export const getStaticProps = BooksServer.getStaticProps
-export default Books
