0cd394f1feddb0b36f91160a84754d398f96dfba
hcorta <hugo.corta@sygris.com>
2023-06-17 20:36:59 +0200

    Add Google AdSense

--- src/frontend/shared/components/PostPage/PostPage.tsx
+++ src/frontend/shared/components/PostPage/PostPage.tsx
@@ -5,6 +5,7 @@ import { PagePostHeader } from './components'
 // Types
 import { FC } from 'react'
 import { ExtendedRecordMap } from 'notion-types'
+import { Divider } from '@mantine/core'
 
 export interface PagePostProps {
   title: string
@@ -23,6 +24,23 @@ export const PagePost: FC<PagePostProps> = ({
     <Page title={title} description={description} showHeader={false}>
       <PagePostHeader title={title} description={description} cover={cover} />
       <NotionRenderer recordMap={recordMap} />
+
+      <Divider />
+
+      <script
+        async
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5123386198574844'
+        crossOrigin='anonymous'
+      ></script>
+      <ins
+        className='adsbygoogle'
+        style={{ display: 'block' }}
+        data-ad-client='ca-pub-5123386198574844'
+        data-ad-slot='1274320855'
+        data-ad-format='auto'
+        data-full-width-responsive='true'
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </Page>
   )
 }
