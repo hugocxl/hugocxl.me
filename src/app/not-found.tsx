f4c88fe6f3c71295a8efdcfa0a430ba69a5570bc
hcorta <corta.hugo@gmail.com>
2023-07-14 19:53:07 +0200

    Fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import { Page } from '@/shared/components'
 
 export default function NotFound() {
   return (
-    <Page title='Not found'>
+    <Page title='Not found' description={'oops'}>
       <p>
         {`It seems you've found something that used to exist, or you spelled something
           wrong. Can you double check that URL?`}
